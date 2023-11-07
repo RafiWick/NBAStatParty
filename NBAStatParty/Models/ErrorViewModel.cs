8de358901c6d9e68de371d8d8a642fbc6b9a96fe
RafiWick <rafiwick@gmail.com>
2023-11-07 13:20:35 -0700

    Add project files.

---
+++ NBAStatParty/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NBAStatParty.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
