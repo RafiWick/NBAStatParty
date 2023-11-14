0ccb3621165b2e83257f522cf522a1af8536c1d4
RafiWick <rafiwick@gmail.com>
2023-11-14 15:43:57 -0700

    team favorites create and delete

--- NBAStatParty/Controllers/TeamsController.cs
+++ NBAStatParty/Controllers/TeamsController.cs
@@ -31,7 +31,7 @@ namespace NBAStatParty.Controllers
                 .Include(t => t.Seasons).ThenInclude(s => s.Records)
                 .FirstOrDefault(t => t.Id == id);
 
-
+            ViewData["Favorites"] = _context.Favorites.Select(f => f.FavoriteId).ToList();
 
             return View(team);
         }
@@ -42,6 +42,17 @@ namespace NBAStatParty.Controllers
         {
             var favorite = new Favorite("TEAM", id);
             _context.Favorites.Add(favorite);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("teams/removefavorite")]
+        public IActionResult RemoveFavorite(string? id)
+        {
+            var favorite = _context.Favorites.FirstOrDefault(f => f.FavoriteId == id);
+            _context.Favorites.Remove(favorite);
+            _context.SaveChanges();
             return Ok();
         }
     }
---
+++ NBAStatParty/Migrations/20231114222246_add-favorites.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NBAStatParty.Migrations
+{
+    /// <inheritdoc />
+    public partial class addfavorites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "favorites",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type = table.Column<string>(type: "text", nullable: false),
+                    favorite_id = table.Column<string>(type: "text", nullable: false),
+                    rating = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_favorites", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "favorites");
+        }
+    }
+}
